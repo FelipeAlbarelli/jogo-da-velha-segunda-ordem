137ba7bb0cfa952c05647a5cd10ab6067b483d91
Albarelli <felipesalbarelli@gmail.com>
2024-04-01 20:33:20 -0300

    comeco de shaders em cells

--- src/lib/3d/tic-tac-toe-objs/Cell3D.svelte
+++ src/lib/3d/tic-tac-toe-objs/Cell3D.svelte
@@ -1,8 +1,28 @@
 <script lang="ts">
     import { T  } from  '@threlte/core'
+	import { Vector2, Vector3 } from 'three';
     export let y = 0 , z = 0;
 
     export let size = 9;
+  const pulsePosition = new Vector3()
+  let vertexShader = `
+        void main() {
+            gl_FragColor = vec4(1.0,0.0,1.0,1.0);
+        }
+  `
+    const frag = ` 
+        #ifdef GL_ES
+        precision mediump float;
+        #endif
+
+        uniform float u_time;
+
+        void main() {
+            gl_FragColor = vec4(1.0, 1, 1.0, 1.0);
+        }
+
+     `
+
 </script>
 
 
@@ -12,6 +32,15 @@
 >
   <T.MeshStandardMaterial  />
   <T.BoxGeometry args={[0.5, size, size]} />
-  <!-- <T.ShaderMaterial
-  /> -->
+  <T.ShaderMaterial
+    uniforms={{
+      pulseTimer: {
+        value: 0
+      },
+      pulsePosition: {
+        value: pulsePosition
+      }
+    }}
+    fragmentShader={frag}
+  />
 </T.Mesh>
