0a3adea9a808c15a0c25ff905c63ebd92652e87d
Albarelli <felipesalbarelli@gmail.com>
2024-04-01 22:28:26 -0300

    vertex shader ok

--- src/lib/3d/tic-tac-toe-objs/Cell3D.svelte
+++ src/lib/3d/tic-tac-toe-objs/Cell3D.svelte
@@ -2,10 +2,12 @@
     import { T  } from  '@threlte/core'
 	import { quadOut } from 'svelte/easing';
 	import { tweened } from 'svelte/motion';
-	import { Vector2, Vector3 } from 'three';
+	import { BoxGeometry, ShaderMaterial, TorusGeometry, Vector2, Vector3 } from 'three';
 	import { debugMsg } from '../store';
 
     import fragShader from '../../../shaders/frag.glsl?raw'
+    import vertexShader from '../../../shaders/vertex.glsl?raw'
+	import TabsNavBar from '../../../navigation/TabsNavBar.svelte';
     export let y = 0 , z = 0;
 
     export let size = 9;
@@ -21,24 +23,19 @@
     pulseTimer.subscribe( s => {
         debugMsg.update( prev => ({...prev, pulseTimer : s}) )
     })
-  let vertexShader = `
-        void main() {
-            gl_FragColor = vec4(1.0,0.0,1.0,1.0);
-        }
-  `
-    const frag = ` 
-        #ifdef GL_ES
-        precision mediump float;
-        #endif
-        uniform vec2 u_resolution;  // Canvas size (width,height)
-        uniform vec2 u_mouse;       // mouse position in screen pixels
-        uniform float pulseTimer;       // Time in seconds since load
+    const geometry = new BoxGeometry(0.5 , 5 , 5) 
+    const geometry2 = new TorusGeometry(3 , 2 , 10 ) 
 
-        void main() {
-            gl_FragColor = vec4(abs(sin(pulseTimer)),0.0,0.0,1.0);
-        }
+    geometry.computeVertexNormals()
+    geometry2.computeVertexNormals()
 
-     `
+    let shader : ShaderMaterial
+
+    $: {
+        if (shader) {
+        shader.vertexShader = vertexShader
+        } 
+    } 
 
 </script>
 
@@ -46,10 +43,13 @@
 <T.Mesh
     position.y={y }
     position.z={ z  }
+    geometry={geometry}
 >
-  <T.MeshStandardMaterial  />
-  <T.BoxGeometry args={[0.5, size, size]} />
+
   <T.ShaderMaterial
+    
+    bind:ref={shader}
+
     uniforms={{
       pulseTimer: {
         value: 0
@@ -59,6 +59,7 @@
       }
     }}
     uniforms.pulseTimer.value={$pulseTimer  ?? 0 }
+    vertextShader={vertexShader}
     fragmentShader={fragShader}
   />
 </T.Mesh>
--- src/shaders/frag.glsl
+++ src/shaders/frag.glsl
@@ -5,7 +5,14 @@ uniform vec2 u_resolution;// Canvas size (width,height)
 uniform vec2 u_mouse;// mouse position in screen pixels
 uniform float pulseTimer;// Time in seconds since load
 
+varying vec2 vUv;
+varying vec3 vPosition;
+
 void main(){
-    gl_FragColor=vec4(abs(sin(pulseTimer)),0.,0.,1.);
+    gl_FragColor=vec4(
+        abs(sin(vPosition.x+pulseTimer)),
+        abs(sin(vPosition.y+pulseTimer)),
+        abs(sin(vPosition.z+pulseTimer)),
+    1);
 }
 
